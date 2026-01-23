7e9ee6d24ecf7abfa911b678a1d960b5a1abd277
zacharybaehr101 <zacharybaehr@gmail.com>
2026-01-23 16:26:17 -0600

    Refactor ReactDOM import and update App path

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import App from "./App.js";
+import "./styles.css";
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
